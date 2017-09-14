ba15d6d0f67507505be63fd3d54110ab4f71b7b7
Cano <mcano4216@greenville.k12.sc.us>
2017-09-14 09:25:45 -0400

    Added outcome when player wins/loses

--- guess_a_number.py
+++ guess_a_number.py
@@ -4,13 +4,19 @@ import random
 
 low = 1
 high = 100
+limit = 10
+
+#start game
 
 rand = random.randint(1,100)
 print("I'm thinking of a number from " + str(low)+ " to " + str(high) + ".");
 
 guess = -1
+tries = 0
 
-while guess != rand:
+#play the game
+
+while guess != rand and tries < limit:
     guess = input("Take a guess: ")
     guess = int(guess)
     
@@ -18,7 +24,15 @@ while guess != rand:
         print("You guessed too low.")
     elif guess > rand:
         print("You guessed too high.")
-    else:
-        print("You got it!")
+
+    tries += 1
+        
+
+#tell player outcome
+    
+if guess == rand:
+    print("You win!")
+else:
+    print("You lose. The number I was thinking of was " + str(rand) + ".")
 
 print("Game over")
