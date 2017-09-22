324fcc48b344e223a1cc958245b44951910821a7
Cano <mcano4216@greenville.k12.sc.us>
2017-09-22 09:30:09 -0400

    Final version

--- guess_a_number.py
+++ guess_a_number.py
@@ -3,16 +3,8 @@ import random
 #config
 
 low = 1
-high = 100
+high = 10
 limit = 10
-
-#start game
-
-rand = random.randint(1,100)
-print("I'm thinking of a number from " + str(low)+ " to " + str(high) + ".");
-
-guess = -1
-tries = 0
 
 #helper function
 
@@ -25,25 +17,51 @@ def get_guess():
             return g
         else:
             print("You must enter a number.")
+            
+def play_again():
+    while True:
+        decision = input("Would you like to play again? (y/n)")
 
-#play the game
+        if decision == "y" or decision == "yes":
+            return True
+        elif decision == "n" or decision == "no":
+            return False
+        print("I don't understand. Please answer either 'y' or 'n'.")
 
-while guess != rand and tries < limit:
-    guess = get_guess()
-    
-    if guess < rand:
-        print("You guessed too low.")
-    elif guess > rand:
-        print("You guessed too high.")
+again = True
 
-    tries += 1
+
+
+while again:
+
+    #start game
+
+    rand = random.randint(1,100)
+    print("I'm thinking of a number from " + str(low)+ " to " + str(high) + ".");
+
+    guess = -1
+    tries = 0
+
+    #play the game
+
+    while guess != rand and tries < limit:
+        guess = get_guess()
         
+        if guess < rand:
+            print("You guessed too low.")
+        elif guess > rand:
+            print("You guessed too high.")
 
-#tell player outcome
-    
-if guess == rand:
-    print("You win!")
-else:
-    print("You lose. The number I was thinking of was " + str(rand) + ".")
+        tries += 1
+            
 
-print("Game over")
+    #tell player outcome
+        
+    if guess == rand:
+        print("You win!")
+    else:
+        print("You lose. The number I was thinking of was " + str(rand) + ".")
+
+    print("Game over")
+
+print("Goodbye.")
